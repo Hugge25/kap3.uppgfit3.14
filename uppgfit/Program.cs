553ac1c470b972fbc3405b3be73eb0681c7ec3fa
Alka04 <alka04006@utb.vaxjo.se>
2022-09-23 11:15:05 +0200

    Add project files.

---
+++ uppgfit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace uppgfit
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+            Console.WriteLine("Har du gått ut gymnasiet? Svara ja eller nej");
+            string skola = Console.ReadLine();
+
+            if (ålder <= 22 && skola == "ja")
+            {
+                Console.WriteLine("Du är anställd");
+            }
+
+            else
+            {
+                Console.WriteLine("Du är inte anställd");
+            }
+        }
+    }
+}
